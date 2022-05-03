095c727bd99970ef9453894627540ecbea88ab7a
fabiojrfla <fabiojrfla@gmail.com>
2022-05-03 13:44:50 -0300

    Listagem de galpões cadastrados na tela inicial

--- spec/system/user_view_homepage_spec.rb
+++ spec/system/user_view_homepage_spec.rb
@@ -3,6 +3,24 @@ require 'rails_helper'
 describe 'Usuário visita tela inicial' do
   it 'e vê o nome do app' do
     visit('/')
+
     expect(page).to have_content('Galpões & Estoque')
+  end
+
+  it 'e vê os galpões cadastrados' do
+    Warehouse.create(name: 'Rio', code: 'SDU', city: 'Rio de Janeiro', area: 60_000)
+    Warehouse.create(name: 'Mossoro', code: 'MVF', city: 'Mossoro', area: 50_000)
+
+    visit('/')
+
+    expect(page).to have_content('Rio')
+    expect(page).to have_content('Código: SDU')
+    expect(page).to have_content('Cidade: Rio de Janeiro')
+    expect(page).to have_content('60000m²')
+
+    expect(page).to have_content('Mossoro')
+    expect(page).to have_content('Código: MVF')
+    expect(page).to have_content('Cidade: Mossoro')
+    expect(page).to have_content('50000m²')
   end
 end
