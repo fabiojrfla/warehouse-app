0a89a6bba3b46cee6a5f0cab46ede776bceb3df0
fabiojrfla <fabiojrfla@gmail.com>
2022-05-04 10:35:00 -0300

    Link para voltar da página de detalhes para a página inicial

--- spec/system/user_view_homepage_spec.rb
+++ spec/system/user_view_homepage_spec.rb
@@ -2,33 +2,33 @@ require 'rails_helper'
 
 describe 'Usuário visita tela inicial' do
   it 'e vê o nome do app' do
-    visit('/')
+    visit root_path
 
-    expect(page).to have_content('Galpões & Estoque')
+    expect(page).to have_content 'Galpões & Estoque'
   end
 
   it 'e vê os galpões cadastrados' do
     Warehouse.create(name: 'Rio', code: 'SDU', city: 'Rio de Janeiro', area: 60_000)
     Warehouse.create(name: 'Mossoro', code: 'MVF', city: 'Mossoro', area: 50_000)
 
-    visit('/')
+    visit root_path
 
-    expect(page).not_to have_content('Não existem galpões cadastrados')
+    expect(page).not_to have_content 'Não existem galpões cadastrados'
 
-    expect(page).to have_content('Rio')
-    expect(page).to have_content('Código: SDU')
-    expect(page).to have_content('Cidade: Rio de Janeiro')
-    expect(page).to have_content('60000m²')
+    expect(page).to have_content 'Rio'
+    expect(page).to have_content 'Código: SDU'
+    expect(page).to have_content 'Cidade: Rio de Janeiro'
+    expect(page).to have_content '60000m²'
 
-    expect(page).to have_content('Mossoro')
-    expect(page).to have_content('Código: MVF')
-    expect(page).to have_content('Cidade: Mossoro')
-    expect(page).to have_content('50000m²')
+    expect(page).to have_content 'Mossoro'
+    expect(page).to have_content 'Código: MVF'
+    expect(page).to have_content 'Cidade: Mossoro'
+    expect(page).to have_content '50000m²'
   end
 
   it 'e não existem galpões cadastrados' do
-    visit('/')
+    visit root_path
 
-    expect(page).to have_content('Não existem galpões cadastrados')
+    expect(page).to have_content 'Não existem galpões cadastrados'
   end
 end
--- spec/system/user_view_warehouse_details_spec.rb
+++ spec/system/user_view_warehouse_details_spec.rb
@@ -2,24 +2,30 @@ require 'rails_helper'
 
 describe 'Usuário vê detalhes de um galpão' do
   it 'e vê informações adicionais' do
-    Warehouse.create(
-      name: 'Aeroporto SP',
-      code: 'GRU',
-      city: 'Guarulhos',
-      area: 100_000,
-      address: 'Avenida do Aeroporto, 1000',
-      cep: '15000-000',
-      description: 'Galpão destinado para cargas internacionais'
-    )
+    Warehouse.create(name: 'Aeroporto SP', code: 'GRU', city: 'Guarulhos', area: 100_000,
+                     address: 'Avenida do Aeroporto, 1000', cep: '15000-000',
+                     description: 'Galpão destinado para cargas internacionais')
 
-    visit('/')
-    click_on('Aeroporto SP')
+    visit root_path
+    click_on 'Aeroporto SP'
 
-    expect(page).to have_content('Galpão GRU')
-    expect(page).to have_content('Nome: Aeroporto SP')
-    expect(page).to have_content('Cidade: Guarulhos')
-    expect(page).to have_content('Área: 100000m²')
-    expect(page).to have_content('Endereço: Avenida do Aeroporto, 1000 CEP: 15000-000')
-    expect(page).to have_content('Galpão destinado para cargas internacionais')
+    expect(page).to have_content 'Galpão GRU'
+    expect(page).to have_content 'Nome: Aeroporto SP'
+    expect(page).to have_content 'Cidade: Guarulhos'
+    expect(page).to have_content 'Área: 100000m²'
+    expect(page).to have_content 'Endereço: Avenida do Aeroporto, 1000 CEP: 15000-000'
+    expect(page).to have_content 'Galpão destinado para cargas internacionais'
+  end
+
+  it 'e volta pra tela inicial' do
+    Warehouse.create(name: 'Aeroporto SP', code: 'GRU', city: 'Guarulhos', area: 100_000,
+                     address: 'Avenida do Aeroporto, 1000', cep: '15000-000',
+                     description: 'Galpão destinado para cargas internacionais')
+
+    visit root_path
+    click_on 'Aeroporto SP'
+    click_on 'Voltar'
+
+    expect(current_path).to eq root_path
   end
 end
